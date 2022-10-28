007f706a3be20fa707a38b807c96ba0417dcca73
Josh Fraser <josh_fraser@ionburst.io>
2022-10-28 21:40:18 +0100

    latest gitlab backup script

--- gitlab-backup/gitlab-backup-ionfs.sh
+++ gitlab-backup/gitlab-backup-ionfs.sh
@@ -7,18 +7,22 @@ date=$(date "+%Y%m%d-%H%M%S")
 name="$date"_gitlab_backup.tar
 data_path=/var/opt/gitlab/backups/
 config_path=/etc/gitlab/
+user=$(whoami)
 
 ## create backups
-gitlab-backup create BACKUP="$date"
-tar -cvf "$data_path/$date"-config.tar "$config_path"
+sudo gitlab-backup create BACKUP="$date" SKIP="artifacts"
+sudo mv "$data_path/$name" /tmp/
+sudo chown "$user:$user" /tmp/"$name"
+
+sudo tar -cf "/tmp/$date"_gitlab_config.tar "$config_path"
+sudo chown "$user:$user" "/tmp/$date"_gitlab_config.tar
 
 ## upload to IBC S6
-ionfs put "$data_path/$name" ion://
-ionfs put "$data_path/$date"_gitlab_config.tar ion://
-
+ionfs put "/tmp/$name" ion://gitlab-backups/
+ionfs put "/tmp/$date"_gitlab_config.tar ion://gitlab-backups/
 
 ## verify and delete local copies
-ionfs ls ion://
+ionfs ls ion://gitlab-backups/
 
-rm -f "$data_path/$name"
-rm -f "$data_path/$date"_gitlab_config.tar
+rm -f "/tmp/$name"
+rm -f "/tmp/$date"_gitlab_config.tar
