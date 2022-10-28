2f22b048416eefc20c92228a0fc757d0876c0613
Josh Fraser <josh_fraser@ionburst.io>
2022-10-28 17:48:57 +0100

    update gitlab config backup name

--- gitlab-backup/gitlab-backup-ionfs.sh
+++ gitlab-backup/gitlab-backup-ionfs.sh
@@ -14,11 +14,11 @@ tar -cvf "$data_path/$date"-config.tar "$config_path"
 
 ## upload to IBC S6
 ionfs put "$data_path/$name" ion://
-ionfs put "$data_path/$date"-config.tar ion://
+ionfs put "$data_path/$date"_gitlab_config.tar ion://
 
 
 ## verify and delete local copies
 ionfs ls ion://
 
 rm -f "$data_path/$name"
-rm -f "$data_path/$date"-config.tar
+rm -f "$data_path/$date"_gitlab_config.tar
